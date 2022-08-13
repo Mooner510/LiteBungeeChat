31069252984ea5e9b909a667e00e5d726daee0ae
Mooner510 <lmck510@gmail.com>
2022-08-13 17:59:56 +0900

    Moved ChatDB to BungeeAPI

--- src/main/java/org/mooner/litebungeechat/LiteBungeeChat.java
+++ src/main/java/org/mooner/litebungeechat/LiteBungeeChat.java
@@ -7,9 +7,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.mooner.litebungeechat.db.ChatDB;
 import org.mooner.moonerbungeeapi.api.BungeeAPI;
 import org.mooner.moonerbungeeapi.api.Rank;
+import org.mooner.moonerbungeeapi.db.ChatDB;
 
 import static org.mooner.moonerbungeeapi.api.BungeeAPI.sendBungeeMessage;
 
@@ -22,7 +22,6 @@ public final class LiteBungeeChat extends JavaPlugin implements Listener {
         plugin = this;
         getLogger().info("Plugin Enabled!");
         port = this.getServer().getPort();
-        ChatDB.init = new ChatDB();
         Bukkit.getPluginManager().registerEvents(this, this);
         //        this.getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new MessageListener());
         // Plugin startup logic
